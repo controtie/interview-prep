9b6b0f76ac6940e2be21526f72f186b0b1ffb6d5
controtie <dylan.ad.tran@gmail.com>
2017-09-12 19:46:47 -0700

    add addEdge, isTraversable methods

--- graph/unidirectional-graph.js
+++ graph/unidirectional-graph.js
@@ -4,23 +4,46 @@ const UnidirectionalGraph = function() {
   this.length = 0;
 };
 
-UnidirectionalGraph.prototype.add = function(node) {
-  if (this.nodes[node.value] !== undefined) {
+UnidirectionalGraph.prototype.add = function(value) {
+  const newNode = Node(value);
+  if (this.nodes[value] !== undefined) {
     return false;
   }
   if (this.length === 0) {
-    this.root = node;
+    this.root = newNode;
   }
-  this.nodes[node.value] = node;
+  this.nodes[value] = newNode;
   this.length++;
 };
 
 UnidirectionalGraph.prototype.addEdge = function(startNodeValue, endNodeValue) {
-  if (this.nodes[startNode] === undefined ||
-    this.nodes[endNode] === undefined) {
+  const startNode = this.nodes[startNodeValue];
+  const endNode = this.nodes[endNodeValue];
+  if (startNode === undefined ||
+    endNode === undefined) {
     return false;
   }
-  this.nodes[startNode].friends = [];
+  startNode.friends.push(endNode);
+}
+
+UnidirectionalGraph.prototype.isTraversable = function(startNodeValue, endNodeValue) {
+  const startNode = this.nodes[startNodeValue];
+  const endNode = this.nodes[endNodeValue];
+  if (startNode === undefined ||
+    endNode === undefined) {
+    return false;
+  }
+
+  const findNode = (currentNode) => {
+    return currentNode.friends.filter((friendNode) => {
+      if (friendNode.value === endNodeValue) {
+        return true;
+      } else {
+        return findNode(friendNode);
+      }
+    })
+  };
+  return findNode(startNode).length > 0;
 }
 
 const Node = function(value) {
--- graph/unidirectional-graph.test.js
+++ graph/unidirectional-graph.test.js
@@ -11,36 +11,47 @@ describe('unidirectional-graph.js', () => {
 
   describe('UnidirectionalGraph add', () => {
     it('on first invocation, sets passed node as root', () => {
-      const node = {value: 10};
-      Graph.add(node);
-      expect(Graph.root).toBe(node);
+      Graph.add(5);
+      expect(Graph.root.value).toBe(5);
     });
     it('stores added nodes in a list of references', () => {
-      const one = {value: 1};
-      const two = {value: 2};
-      Graph.add(one);
-      Graph.add(two);
-      expect(Graph.nodes[1]).toBe(one);
-      expect(Graph.nodes[2]).toBe(two);
-      Graph.add(one);
+      Graph.add(1);
+      Graph.add(2);
+      expect(Graph.nodes[1].value).toBe(1);
+      expect(Graph.nodes[2].value).toBe(2);
+      Graph.add(1);
       expect(Graph.length).toBe(2);
     });
   });
 
   describe('UnidirectionalGraph addEdge', () => {
-    xit('returns false for two nodes that do not exist in Graph', () => {
-      const one = {value: 1};
-      const two = {value: 2};
-      Graph.add(one);
-      Graph.add(two);
+    it('returns false for two nodes that do not exist in Graph', () => {
+      Graph.add(1);
+      Graph.add(2);
       expect(Graph.addEdge(1, 3)).toBe(false);
     })
-    xit('adds an edge from first param to second param', () => {
-      const one = {value: 1};
-      const two = {value: 2};
-      Graph.add(one);
-      Graph.add(two);
+    it('adds an edge from first param to second param', () => {
+      Graph.add(1);
+      Graph.add(2);
       Graph.addEdge(1, 2);
+      const secondNode = Graph.nodes[1].friends.find((node) => node.value === 2);
+      expect(secondNode.value).toBe(2);
+      Graph.addEdge(2, 1);
+      const firstNode = Graph.nodes[2].friends.find((node) => node.value === 1);
+      expect(firstNode.value).toBe(1);
+    });
+  });
+
+  describe('UnidirectionalGraph isTraversable', () => {
+    it('return true if two points are connected/traversable', () => {
+      Graph.add(1);
+      Graph.add(2);
+      Graph.add(3);
+      Graph.addEdge(1, 2);
+      expect(Graph.isTraversable(1, 2)).toBe(true);
+      expect(Graph.isTraversable(2, 1)).toBe(false);
+      Graph.addEdge(2, 3);
+      expect(Graph.isTraversable(1, 3)).toBe(true);
     });
   });
 
