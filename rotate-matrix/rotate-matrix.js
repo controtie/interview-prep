fb65ebc1a4f48cc7a1b1af4b89e769c741d251db
controtie <dylan.ad.tran@gmail.com>
2018-08-16 00:40:51 +0900

    add naive solution for rotate matrix in place

--- rotate-matrix/rotate-matrix.js
+++ rotate-matrix/rotate-matrix.js
@@ -22,17 +22,71 @@ const createNewRow = (matrix, index) => {
   return newRow.reverse();
 }
 
-
-
-
 // rotate a matrix _in place_ n times, where each n = 90 degrees clockwise
 
 const rotateMatrixInPlace = (matrix, n = 1) => {
+  const requiresFinalSwap = hasFinalSwap(matrix);
+
+  matrix.forEach((row, index) => {
+    const iteratorOffest = index * 2;
+    for (let i = 0; i < matrix.length - 1 - iteratorOffest; i++) {
+      firstSwap(matrix, i, index);
+      secondSwap(matrix, i, index);
+      thirdSwap(matrix, i, index);
+    }
+  });
+
+  return matrix;
 };
+
+const hasFinalSwap = (matrix) => {
+  return matrix % 2 === 0;
+};
+
+// offset determines which circle we're operating on
+// e.g. a 4x4 grid will have 2 circles which need to be rotated
+// a 6x6 grid will have 3 circles that need to be rotated
+const firstSwap = (inputMatrix, i, offset = 0) => {
+  // 0, i and i, length
+
+  let temp = inputMatrix[0 + offset][i + offset];
+  let length = inputMatrix.length - 1;
+
+  inputMatrix[0 + offset][i + offset] = inputMatrix[i + offset][length - offset];
+  inputMatrix[i + offset][length - offset] = temp;
+
+  return inputMatrix;
+}
+
+const secondSwap = (inputMatrix, i, offset = 0) => {
+  // 0, i and len - i, 0
+  let temp = inputMatrix[0 + offset][i + offset];
+  let length = inputMatrix.length - 1;
+
+  inputMatrix[0 + offset][i + offset] = inputMatrix[length - i - offset][0 + offset]
+  inputMatrix[length - i - offset][0 + offset] = temp;
+
+  return inputMatrix;
+}
+
+const thirdSwap = (inputMatrix, i, offset = 0) => {
+  // len - 1, 0 and len, len - i
+  let length = inputMatrix.length - 1;
+  let temp = inputMatrix[length - i - offset][0 + offset];
+
+  inputMatrix[length - i - offset][0 + offset] = inputMatrix[length - offset][length - i - offset]
+  inputMatrix[length - offset][length - i - offset] = temp;
+
+  return inputMatrix;
+}
+
 
 module.exports = {
   rotateMatrix,
   createNewRow,
   rotateMatrixInPlace,
+  firstSwap,
+  secondSwap,
+  thirdSwap,
 };
 
--- rotate-matrix/rotate-matrix.test.js
+++ rotate-matrix/rotate-matrix.test.js
@@ -1,5 +1,9 @@
 const {
   rotateMatrix,
+  rotateMatrixInPlace,
+  firstSwap,
+  secondSwap,
+  thirdSwap,
   createNewRow,
 }= require('./rotate-matrix');
 
@@ -65,3 +69,192 @@ describe('RotateMatrix', () => {
     expect(computedOutput).toEqual(expectedOutput);
   });
 });
+
+describe('RotateMatrixInPlace', () => {
+  it('firstSwap should swap [0, i] and [i, matrixLength] positions', () => {
+    const input = [
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [0, 0, 0, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const output = firstSwap(input.slice(), 0);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it('firstSwap should swap [0, i] and [i, matrixLength] positions for an entire row', () => {
+    const input = [
+      [1, 1, 1, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [0, 0, 0, 1],
+      [0, 0, 0, 1],
+      [0, 0, 0, 1],
+      [0, 0, 0, 0]
+    ];
+
+    const output1 = firstSwap(input.slice(), 0);
+    const output2 = firstSwap(output1.slice(), 1);
+    const output3 = firstSwap(output2.slice(), 2);
+    expect(output3).toEqual(expectedOutput);
+  });
+
+  it('firstSwap should swap [0, i] and [i, matrixLength] positions for an entire row according to offsets', () => {
+    const input = [
+      [1, 1, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [1, 1, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const output1 = firstSwap(input.slice(), 0, 1);
+    expect(output1).toEqual(expectedOutput);
+  });
+
+  it('secondSwap should swap [0, i] and [matrixLength - i, 0] positions for a row', () => {
+    const input = [
+      [1, 1, 1, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0]
+    ];
+
+    const output1 = secondSwap(input.slice(), 0);
+    const output2 = secondSwap(output1.slice(), 1);
+    const output3 = secondSwap(output2.slice(), 2);
+    expect(output3).toEqual(expectedOutput);
+  });
+
+  it('secondSwap should swap [0, i] and [matrixLength - i, 0] positions for a row, according to offset', () => {
+    const input = [
+      [1, 1, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [1, 1, 1, 0],
+      [0, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const output1 = secondSwap(input.slice(), 0, 1);
+    expect(output1).toEqual(expectedOutput);
+  });
+
+  it('thirdSwap should swap [matrixLength -1, matrixLength] and [matrixLength - i, 0] positions for a row', () => {
+
+    const input = [
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 1, 1, 1]
+    ];
+
+    const output1 = thirdSwap(input.slice(), 0);
+    const output2 = thirdSwap(output1.slice(), 1);
+    const output3 = thirdSwap(output2.slice(), 2);
+    expect(output3).toEqual(expectedOutput);
+  });
+
+
+  it('thirdSwap should swap [matrixLength -1, matrixLength] and [matrixLength - i, 0] positions for a row, according to offsets', () => {
+    const input = [
+      [1, 1, 1, 0],
+      [0, 0, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [1, 1, 1, 0],
+      [0, 0, 0, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0]
+    ];
+
+    const output1 = thirdSwap(input.slice(), 0, 1);
+    expect(output1).toEqual(expectedOutput);
+  });
+
+
+  it('should rotate a 4x4 array 90 degrees', () => {
+    const input = [
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 1, 0, 0]
+    ];
+
+    const expectedOutput = [
+      [0, 1, 0, 1],
+      [1, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0]
+    ];
+
+    const computedOutput = rotateMatrixInPlace(input);
+
+    expect(computedOutput).toEqual(expectedOutput);
+  });
+
+
+  it('should rotate a 5x5 array 90 degrees', () => {
+    const input = [
+      [1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 0],
+      [0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+    ];
+
+    const expectedOutput = [
+      [0, 0, 1, 0, 1],
+      [0, 1, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 0],
+    ];
+
+    const computedOutput = rotateMatrixInPlace(input);
+
+    expect(computedOutput).toEqual(expectedOutput);
+  });
+
+});
+
